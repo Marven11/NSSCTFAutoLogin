80809b70095b2d4ada61a74d2919ad81bf372dd0
Marven11 <110723864+Marven11@users.noreply.github.com>
2023-07-16 00:37:07 +0800

    Update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-import asyncio
 import logging
-import httpx
+import requests
 
 USER_AGENT = (
     "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 "
@@ -9,56 +8,73 @@ USER_AGENT = (
 )
 
 
-async def login():
-    """使用账号密码登陆
+def login(session: requests.Session):
+    """使用session登陆，并将token写入session中
+
+    Args:
+        session (requests.Session): requests的session
+    """
+    resp = session.post(
+        "https://www.nssctf.cn/api/user/login/",
+        headers={"User-Agent": USER_AGENT},
+        data={
+            "username": os.environ["NSSCTF_USERNAME"],
+            "password": os.environ["NSSCTF_PASSWORD"],
+        },
+    )
+    data = resp.json()
+    assert data["code"] == 200
+    session.cookies["token"] = data["data"]["token"]
+
+
+def signin(session: requests.Session):
+    """进行签到（虽然只有两三个硬币
+
+    Args:
+        session (requests.Session): 使用的session
 
     Returns:
-        _type_: _description_
+        bool: 是否签到成功
     """
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(
-            "https://www.nssctf.cn/api/user/login/",
-            headers={"User-Agent": USER_AGENT},
-            data={
-                "username": os.environ["NSSCTF_USERNAME"],
-                "password": os.environ["NSSCTF_PASSWORD"],
-            },
-        )
-        cookies = dict(resp.cookies)
-        cookies["token"] = resp.json()["data"]["token"]
-        return cookies
+    resp = session.post(
+        "https://www.nssctf.cn/api/user/clockin/",
+        headers={"User-Agent": USER_AGENT},
+    )
+    logging.warning(resp.text)
+    data = resp.json()
+    return data["code"] == 200
 
 
-async def signin(cookies):
-    async with httpx.AsyncClient(cookies=cookies) as client:
-        resp = await client.post(
-            "https://www.nssctf.cn/api/user/clockin/",
-            headers={"User-Agent": USER_AGENT},
-        )
-        logging.warning(resp.text)
-        data = resp.json()
-        return data["code"] == 200
+def coin_num(session: requests.Session) -> int:
+    """获取硬币数量
+
+    Args:
+        session (requests.Session): 使用的session
+
+    Returns:
+        int: 硬币数量，失败时为None
+    """
+    resp = session.get(
+        "https://www.nssctf.cn/api/user/info/opt/setting/",
+        headers={"User-Agent": USER_AGENT},
+    )
+    logging.warning(resp.text)
+    data = resp.json()
+    assert data["code"] == 200
+    num = data.get("data", {}).get("coin", None)
+    assert num is not None
+    return num
 
 
-async def coin_num(cookies):
-    async with httpx.AsyncClient(cookies=cookies) as client:
-        resp = await client.get(
-            "https://www.nssctf.cn/api/user/info/opt/setting/",
-            headers={"User-Agent": USER_AGENT},
-        )
-        logging.warning(resp.text)
-
-        data = resp.json()
-
-        if data["code"] != 200:
-            return None
-        return data.get("data", {}).get("coin", None)
-
-async def main():
-    cookies = await login()
-    await signin(cookies)
-    num = await coin_num(cookies)
+def main():
+    """登陆，签到并打印coin数量
+    """
+    session = requests.Session()
+    login(session)
+    signin(session)
+    num = coin_num(session)
     print(num)
 
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
